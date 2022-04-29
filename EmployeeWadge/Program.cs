a56e1bbadcd55523bb136f94e27f8794fd93086c
Adi438 <mishraaaditya54@gmail.com>
2022-04-29 17:34:37 +0530

    [Aditya] UC1 program

--- EmployeeWadge/Program.cs
+++ EmployeeWadge/Program.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeWadge
+namespace UC1_Present_Absent
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employeewadge Problem");
-            Console.ReadLine();
-       }
+            int FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            
+        }   
+         
+           
+                
+
     }
 }
