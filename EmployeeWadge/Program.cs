1ecfc96c263ae33841509aeaaf691a68a626f59e
Adi438 <mishraaaditya54@gmail.com>
2022-05-01 02:07:46 +0530

    Add PartTime Job

--- EmployeeWadge/Program.cs
+++ EmployeeWadge/Program.cs
@@ -11,21 +11,22 @@ namespace EmployeeWadge
         static void Main(string[] args)
         {
 
-            int Full_TIME = 1;
+            int Full_TIME = 2;
+            int PART_TIME = 1;
             int EMP_EATE_PER_HOUR = 20;
 
             int emphrs = 0;
             int empWage= 0;
             Random random = new Random();
             int empcheck = random.Next(0, 2);
-            if (empcheck == Full_TIME)
+            if (empcheck == PART_TIME )
             {
-                emphrs = 8;
+                emphrs = 4;
 
             }
-            else  
+            else if (empcheck == Full_TIME)
             {
-                emphrs = 0;
+                emphrs = 8;
             }
             empWage = emphrs * EMP_EATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
